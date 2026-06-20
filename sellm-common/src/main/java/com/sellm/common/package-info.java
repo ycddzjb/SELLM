f5bc0850ba2725fedcc0d3686101db5d9725db05
chenzhongshan <ycddzjb@163.com>
2026-06-20 21:54:33 +0800

    feat(p0): Maven reactor parent + sellm-common 空壳模块

---
+++ sellm-common/src/main/java/com/sellm/common/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * sellm-common: 共享能力层(P0 空壳占位)。
+ * P1 阶段将从 sellm-agent-assessment 逐步抽离:
+ * - Result / GlobalExceptionHandler (统一返回/异常)
+ * - FieldCipher (PII 加密)
+ * - AccessGuard (行级权限)
+ * - JWT / SecurityConfig 基础
+ */
+package com.sellm.common;
